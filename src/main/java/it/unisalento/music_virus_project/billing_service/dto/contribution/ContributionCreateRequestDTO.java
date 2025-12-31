5dbfe64ccf1be5afbbd9a8a3dff532585dcabcc1
Andrea Barone <101096469+bandrea01@users.noreply.github.com>
2025-12-31 16:25:37 +0100

    refactor: move DTOs for contribution creation with request and response models in billing repository

---
+++ src/main/java/it/unisalento/music_virus_project/billing_service/dto/contribution/ContributionCreateRequestDTO.java
@@ -0,0 +1,46 @@
+package it.unisalento.music_virus_project.billing_service.dto.contribution;
+
+import it.unisalento.music_virus_project.billing_service.domain.enums.ContributionVisibility;
+
+import java.math.BigDecimal;
+
+public class ContributionCreateRequestDTO {
+
+    private String fundraisingId;
+    private String userId;
+    private BigDecimal amount;
+    private ContributionVisibility contributionVisibility;
+
+    public ContributionCreateRequestDTO(String fundraisingId, String userId, BigDecimal amount, ContributionVisibility contributionVisibility) {
+        this.fundraisingId = fundraisingId;
+        this.userId = userId;
+        this.amount = amount;
+        this.contributionVisibility = contributionVisibility;
+    }
+
+    public ContributionCreateRequestDTO() {}
+    public String getFundraisingId() {
+        return fundraisingId;
+    }
+    public void setFundraisingId(String fundraisingId) {
+        this.fundraisingId = fundraisingId;
+    }
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public BigDecimal getAmount() {
+        return amount;
+    }
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+    public ContributionVisibility getContributionVisibility() {
+        return contributionVisibility;
+    }
+    public void setContributionVisibility(ContributionVisibility visibility) {
+        this.contributionVisibility = visibility;
+    }
+}
---
+++ src/main/java/it/unisalento/music_virus_project/billing_service/dto/contribution/ContributionCreateResponseDTO.java
@@ -0,0 +1,38 @@
+package it.unisalento.music_virus_project.billing_service.dto.contribution;
+
+import it.unisalento.music_virus_project.billing_service.domain.enums.ContributionStatus;
+import it.unisalento.music_virus_project.billing_service.domain.enums.ContributionVisibility;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public class ContributionCreateResponseDTO {
+
+    private String id;
+    private String fundraisingId;
+    private String fanId;
+    private BigDecimal amount;
+    private ContributionVisibility contributionVisibility;
+    private ContributionStatus contributionStatus;
+    private Instant createdAt;
+    private Instant lastUpdate;
+
+    public ContributionCreateResponseDTO() {}
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getFundraisingId() { return fundraisingId; }
+    public void setFundraisingId(String eventId) { this.fundraisingId = eventId; }
+    public String getFanId() { return fanId; }
+    public void setFanId(String fanId) { this.fanId = fanId; }
+    public BigDecimal getAmount() { return amount; }
+    public void setAmount(BigDecimal amount) { this.amount = amount; }
+    public ContributionVisibility getVisibility() { return contributionVisibility; }
+    public void setVisibility(ContributionVisibility contributionVisibility) { this.contributionVisibility = contributionVisibility; }
+    public ContributionStatus getContributionStatus() { return contributionStatus; }
+    public void setContributionStatus(ContributionStatus status) { this.contributionStatus = status; }
+    public Instant getCreatedAt() { return createdAt; }
+    public void setCreatedAt(Instant createdAt) { this.createdAt = createdAt; }
+    public Instant getLastUpdate() { return lastUpdate; }
+    public void setLastUpdate(Instant updatedAt) { this.lastUpdate = updatedAt; }
+}
